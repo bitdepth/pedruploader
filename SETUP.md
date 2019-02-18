eb4886ab7e3c0ecc8ff3aa65c38d4266fe1c581f
clarco <colin.clark@woodmac.com>
2019-02-18 22:34:50 +0000

    Basic setup for file uploads

---
+++ SETUP.md
@@ -0,0 +1,9 @@
+Install Homebrew
+
+`brew install mongodb`
+
+Start mongo using
+
+`mongod --config /usr/local/etc/mongod.conf`
+
+Download https://robomongo.org/download
--- src/App.js
+++ src/App.js
@@ -4,24 +4,54 @@ import './App.css';
 
 class App extends Component {
 
+    state = {
+        caption: '',
+        photos: []
+    }
+
+    componentDidMount() {
+        axios.get('/api/photos')
+            .then(response => this.setState({photos: response.data}))
+            .catch(error => console.log(error));
+    }
+
     handleFileUpload = (event) => {
-
-        console.log('handleFileUpload', event.target.files[0]);
-
         let data = new FormData();
         data.append('userPhoto', event.target.files[0]);
-        // data.append('name', 'userPhoto');
-        // data.append('description', 'userPhoto');
+        data.append('caption', this.state.caption);
 
         axios.post('/api/photo', data)
             .then(response => console.log(response))
             .catch(error => console.log(error));
     }
 
+    updateCaption = (event) => {
+        this.setState({
+            caption: event.target.value
+        });
+    };
+
+    getFilePath = fileName => `/uploads/${fileName}`;
+
     render() {
         return (
             <div className="App">
-                <input type="file" name="userPhoto" onChange={this.handleFileUpload}/>
+                <div>
+                    <label>Caption</label>
+                    <input type="text" value={this.state.caption} name="caption" onChange={this.updateCaption}/>
+                </div>
+                <div>
+                    <label>upload file</label>
+                    <input type="file" name="userPhoto" onChange={this.handleFileUpload}/>
+                </div>
+
+                {this.state.photos.map(photo => (
+                    <figure key={photo._id}>
+                        <img src={this.getFilePath(photo.fileName)} />
+                            <figcaption>{photo.caption}</figcaption>
+                    </figure>
+                ))}
+
             </div>
         );
     }
